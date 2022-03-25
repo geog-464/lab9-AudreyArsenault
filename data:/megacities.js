392a781f1c2b3860e28c3209a97dcc8242d23a48
AudreyArsenault <90860480+AudreyArsenault@users.noreply.github.com>
2022-03-25 19:59:35 -0400

    Add files via upload

---
+++ data:/megacities.js
@@ -0,0 +1,286 @@
+/*--Audrey Dubord-Arsenault, 2022 */
+var geojsonData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Tokyo",
+        "Latitude": 35.695126
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.75391,
+          37400068
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Delhi",
+        "Latitude": 24.734319
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.334404,
+          28514000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Shanghai",
+        "Latitude": 31.246027
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.483385,
+          25582000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "São Paulo",
+        "Latitude": -1.32311
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -47.504873,
+          21650000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mexico City",
+        "Latitude": 19.371422
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -99.150344,
+          21581000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Cairo",
+        "Latitude": 30.06263
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          31.24967,
+          20076000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Mumbai",
+        "Latitude": 19.10109
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          72.875449,
+          19980000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Beijing",
+        "Latitude": 39.937967
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          116.417592,
+          19618000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Dhaka",
+        "Latitude": 23.7104
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.40744,
+          19578000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Osaka",
+        "Latitude": 34.671654
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          135.501902,
+          19281000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "New York",
+        "Latitude": 40.68295
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.9708,
+          18819000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Karachi",
+        "Latitude": 24.984695
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          67.238928,
+          15400000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Buenos Aires",
+        "Latitude": -34.607357
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -58.454595,
+          14967000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Chongqing",
+        "Latitude": 29.565015
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          106.548238,
+          14838000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Istanbul",
+        "Latitude": 41.239403
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.409152,
+          14751000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Kolkata",
+        "Latitude": 22.550437
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          88.362561,
+          14681000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Manila",
+        "Latitude": 14.6042
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.9822,
+          13482000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Lagos",
+        "Latitude": 6.550449
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          3.574901,
+          13463000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Rio de Janeiro",
+        "Latitude": -22.935024
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -43.518246,
+          13293000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "City": "Tianjin",
+        "Latitude": 39.107355
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          117.185182,
+          13215000
+        ]
+      }
+    }
+  ]
+}
---
+++ js:/main.js
@@ -0,0 +1,54 @@
+/*--Audrey Dubord-Arsenault, 2022 */
+//initialize function called when the script loads
+function initialize(){
+    loadmap();
+};
+
+function onEachFeature(feature, geojsonLayer) {
+	if (feature.properties && feature.properties.popupContent) {
+		geojsonLayer.bindPopup(feature.properties.popupContent);
+	}
+}
+
+//function to create a table with cities and their populations
+function loadmap(){
+
+    //create a basemap style. You can find other options at https://leaflet-extras.github.io/leaflet-providers/preview/
+	var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	});
+
+	//add this basemap style to a JS object, to which you could also add other baselayers. This object is loaded as a basemap selector as seen further down
+	var baseLayers = {
+		"osm": OpenStreetMap_Mapnik
+		//,...
+	};
+
+	// create the map
+	var mymap = L.map('mapdiv', {
+		center: [45.50, -73.58]
+		,zoom: 3
+		,maxZoom: 18
+		,minZoom: 3
+		,layers: OpenStreetMap_Mapnik
+	});
+
+	// parse json object (var geojsonData) and turn into loadable layer
+	geojsonLayer = L.geoJSON(geojsonData, {
+		onEachFeature: onEachFeature
+	});
+
+	//add geojsonData to map
+	geojsonLayer.addTo(mymap);// add json element to map
+
+	//declare basemap selector widget
+	var lcontrol = L.control.layers(baseLayers);
+
+	//add it to the map
+	lcontrol.addTo(mymap);
+};
+
+
+//call the initialize function when the window has loaded
+window.onload = initialize();
